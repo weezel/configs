6c1f457587c4b48cdb9e706e42f74c7f1862c066
Ville Valkonen <weezelding@gmail.com>
2017-04-06 22:50:25 +0300

    git: Include [user] section via .gituser. This way gitconfig can be soft linked directly to repo.

--- initconfigs.sh
+++ initconfigs.sh
@@ -2,7 +2,7 @@
 
 ln -s $HOME/configs/dotvim/ $HOME/.vim
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
+ln -s $HOME/configs/gitconfig $HOME/.gitconfig
 cp $HOME/configs/tmux.conf $HOME/.tmux.conf
-cp $HOME/configs/gitconfig $HOME/.gitconfig
 [[ -r $HOME/.ssh/config ]] || cp $HOME/configs/sshconfig $HOME/.ssh/config
 tic $HOME/configs/mostlike.txt
