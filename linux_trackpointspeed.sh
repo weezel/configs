b28c5b36169e7d47c2cfb6877ef5e56aa54e5750
Ville Valkonen <weezelding@gmail.com>
2017-04-26 22:06:15 +0300

    dir -> {speed,sensitivity}.
    After playing a while, 200,200 combo is the winning one.

--- linux_trackpointspeed.sh
+++ linux_trackpointspeed.sh
@@ -4,5 +4,5 @@ mousedev=$(find /sys/devices/platform/i8042 -name name \
 	| xargs grep -Fl TrackPoint \
 	| sed 's/\/input\/input[0-9]*\/name$//')
 
-echo 255 | sudo tee ${mousedev}
-echo 200 | sudo tee ${mousedev}
+echo 200 | sudo tee ${mousedev}/speed
+echo 200 | sudo tee ${mousedev}/sensitivity
