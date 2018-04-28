75b0f2b73ecfea5cbb8b512f40d13fdd9e089150
Ville Valkonen <weezelding@gmail.com>
2018-04-28 22:39:29 +0300

    This shouldn't be here. Move it under scripts repo.

--- linux_trackpointspeed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mousedev=$(find /sys/devices/platform/i8042 -name name \
-	| xargs grep -Fl TrackPoint \
-	| sed 's/\/input\/input[0-9]*\/name$//')
-
-echo 200 | sudo tee ${mousedev}/speed
-echo 200 | sudo tee ${mousedev}/sensitivity
