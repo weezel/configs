ffdd1664dcfd36da96483f4ad0376182dcd3f029
Ville Valkonen <weezelding@gmail.com>
2019-06-15 22:07:08 +0300

    dotvim/init.sh: Add go-langserv and more verbosity to output.

--- dotvim/init.sh
+++ dotvim/init.sh
@@ -1,26 +1,49 @@
 #!/bin/sh
 
-function python3langserv
-{
-	local readonly _pylspath="$HOME/apps/py3-langserver"
 
+function golangserv
+{
+	echo "### Installing go-language-server..."
+	go get -u github.com/sourcegraph/go-langserver
+	echo "### Done"
+}
+
+function python3_langserv
+{
+	local readonly _pylspath="$HOME/apps/language_servers/py3-langserver"
+
+	echo "### Installing python-language-server in venv..."
 	[[ -d "${_pylspath}" ]] || mkdir -p "${_pylspath}"
 	python3 -m venv "${_pylspath}"
 	cd "${_pylspath}"
 	. bin/activate
-	echo "Installing python-language-server in venv..."
 	bin/pip3 install python-language-server
-	echo "Upgrading pip"
+	echo "### Upgrading pip"
 	bin/pip3 install --upgrade pip
 	deactivate
-	echo "Done"
+	echo "### Done"
 }
 
-curl --create-dirs -fLo autoload/plug.vim \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+function vim_plug
+{
+	echo "### Fetching Plug..."
+	curl --create-dirs -fLo autoload/plug.vim \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	echo "### Done"
+}
 
 case "${1}" in
-pyls)
-	python3langserv
+gols)
+	golangserv
 	;;
+plug)
+	vim_plug
+	;;
+pyls)
+	python3_langserv
+	;;
+*)
+	echo "usage: $0: [gols|plug|pyls]"
+	exit 0
 esac
+
