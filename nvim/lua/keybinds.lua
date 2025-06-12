97798f467342b3b6ea1cc579fabee3d83961b693
Ville Valkonen <weezel@users.noreply.github.com>
2025-06-12 23:06:29 +0300

    Bunch of keybinds.
    - Leader + 4 closes the buffer
    - Alt + f,d for moving between quickfix items
    - Ctrl + Pageup,down for buffer moving
    - F1 to toggle relative and non-relative line numbering
    - Ctrl + q needs still more consideration although now it shows hint
      info
    
    Signed-off-by: Ville Valkonen <weezel@users.noreply.github.com>

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,3 +1,6 @@
+-- Prevent recursive mappings
+local opts = { noremap = true, silent = true }
+
 vim.g.mapleader = ";"
 
 -- Mappings
@@ -13,7 +16,8 @@ end, { desc = "Source init.lua" })
 map("n", "<leader>a", ":g/pattern/s/$/<CR>", opts)
 map("n", "<leader>m", ":g/pattern/normal @q<CR>", opts)
 map("n", "<leader>b", ":e#<CR>", opts)
-map("n", "<leader>,", ":bd<CR>", opts)
+
+map("n", "<leader>4", ":bd<CR>", opts) -- Close buffer
 
 -- Diff mode mappings
 if vim.opt.diff:get() then
@@ -22,7 +26,6 @@ if vim.opt.diff:get() then
   vim.opt.foldminlines = 99999
 end
 
--- Insert mode enhancements
 map("i", "<C-c>", "<Esc>", opts)
 map("n", "<C-CR>", "i<CR><Esc>", opts)
 map("i", "<A-h>", "<Left>", opts)
@@ -33,8 +36,9 @@ map("n", "<C-A-j>", "<PageDown>", opts)
 map("n", "<C-A-k>", "<PageUp>", opts)
 map("i", "<C-A-j>", "<ESC><C-E>i", opts)
 map("i", "<C-A-k>", "<ESC><C-Y>i", opts)
-map("n", "<C-h>", ":bprev<CR>", opts)
-map("n", "<C-l>", ":bnext<CR>", opts)
+
+vim.keymap.set("n", "<C-PageUp>", ":bprev<CR>", { noremap = true, silent = true, desc = "Prev buffer"})
+vim.keymap.set("n", "<C-PageDown>", ":bnext<CR>", { noremap = true, silent = true, desc = "Next buffer"})
 
 -- Readline-like bindings
 map("i", "<C-a>", "<esc>I", opts)
@@ -49,9 +53,8 @@ map("n", "<M-j>", "<C-w>j", opts)
 map("n", "<M-k>", "<C-w>k", opts)
 map("n", "<M-l>", "<C-w>l", opts)
 
--- Quickfix
-map("n", "<C-n>", ":cn<CR>", opts)
-map("n", "<C-p>", ":cp<CR>", opts)
+map("n", "<M-f>", ":cnext<CR>", opts) -- Next Quickfix item
+map("n", "<M-d>", ":cprev<CR>", opts) -- Previous Quickfix item
 
 -- Paste without moving cursor
 map("n", "p", "p`[", { noremap = true })
@@ -60,15 +63,18 @@ map("n", "P", "P`[", { noremap = true })
 -- Auto complete
 vim.api.nvim_set_keymap("i", "<C-Space>", "<C-x><C-o>", { noremap = true, silent = true })
 
--- Select auto completed value
+-- Select auto completed value with enter
 vim.api.nvim_set_keymap("i", "<CR>", "pumvisible() ? '<C-y>' : '<CR>'", { expr = true, noremap = true })
 
 -- Show function signature
-vim.api.nvim_set_keymap("i", "<C-p>", "v:lua.vim.lsp.buf.signature_help()", { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-q>", "v:lua.vim.lsp.buf.signature_help()", { noremap = true, silent = true, expr = true })
 
--- Disable F1 help, use it to toggle fullscreen scrolloff
-map("n", "<F1>", ":let &scrolloff=999-&scrolloff<CR>", opts)
-map("i", "<F1>", "<ESC>:let &scrolloff=999-&scrolloff<CR>a", opts)
+-- F1 toggles between relative and non relative line numbers
+vim.keymap.set("n", "<F1>", function()
+  local is_relative = vim.wo.relativenumber
+  vim.wo.relativenumber = not is_relative
+  vim.wo.number = true
+end, { desc = "Toggle relative line numbers" })
 
 -- Show info about the error
 vim.keymap.set("n", "<C-q>", vim.diagnostic.open_float, { noremap = true, desc = "Show diagnostic" })
